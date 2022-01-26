1d9ad320a899a7ecec2c6d7586e78c6d32bb8b9a
thewildmage <cecilia@hey.com>
2022-01-25 19:55:15 -0700

    first pass at script. successfully sends message to specified channel

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const form = document.querySelector("form");
+const output = document.querySelector("output");
+
+form.addEventListener("submit", event => {
+    let url = constructUrl(form.elements);
+    let body = new FormData();
+
+    body.append("content", form.elements.content.value);
+
+    fetch(url, {
+        body: body,
+        method: "POST"
+    }).then(response => {
+        console.log(response);
+        output.value = `${ response.status }: ${ response.statusText }\n`;
+
+        response.text();
+    }).then(text => {
+        if (text) output.value += text;
+    });
+
+    event.preventDefault();
+});
+
+function constructUrl(elements) {
+    let baseUrl = elements.action.value;
+    let threadId = elements.thread_id.value.trim();
+
+    if (threadId) {
+        return `${ baseUrl }?thread_id=${ threadId }`;
+    }
+
+    return baseUrl;
+}
