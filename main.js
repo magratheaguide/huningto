ce900354a0d9c676c1618e30431227a88c6ac769
thewildmage <cecilia@hey.com>
2022-02-21 19:53:26 -0700

    convert main.js to module

--- main.js
+++ main.js
@@ -1,73 +1,69 @@
-"use strict";
+const form = document.querySelector("form");
+const output = document.querySelector("output");
 
-(function () {
-    const form = document.querySelector("form");
-    const output = document.querySelector("output");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+    let submitButton = form.querySelector("button[type='submit']");
+    let url = constructUrl(form.elements);
+    let body = new FormData();
 
-        let submitButton = form.querySelector("button[type='submit']");
-        let url = constructUrl(form.elements);
-        let body = new FormData();
+    output.value = "";
+    submitButton.setAttribute("disabled", "");
 
-        output.value = "";
-        submitButton.setAttribute("disabled", "");
+    // TODO: Need to teach this how to check dependent fields
+    if (form.elements.buildFrom.value === "json") {
+        body.append("payload_json", form.elements.payload_json.value);
+    } else {
+        body.append("content", form.elements.content.value);
+    }
 
-        // TODO: Need to teach this how to check dependent fields
-        if (form.elements.buildFrom.value === "json") {
-            body.append("payload_json", form.elements.payload_json.value);
-        } else {
-            body.append("content", form.elements.content.value);
-        }
+    fetch(url, {
+        body: body,
+        method: "POST",
+    }).then((response) => {
+        console.log(response);
 
-        fetch(url, {
-            body: body,
-            method: "POST",
-        }).then((response) => {
-            console.log(response);
-
-            displayResponse(response);
-        });
-
-        submitButton.removeAttribute("disabled");
+        displayResponse(response);
     });
 
-    function constructUrl(elements) {
-        let baseUrl = elements.action.value;
-        let threadId = elements.thread_id.value.trim();
+    submitButton.removeAttribute("disabled");
+});
 
-        if (threadId) {
-            return `${baseUrl}?thread_id=${threadId}`;
-        }
+function constructUrl(elements) {
+    let baseUrl = elements.action.value;
+    let threadId = elements.thread_id.value.trim();
 
-        return baseUrl;
+    if (threadId) {
+        return `${baseUrl}?thread_id=${threadId}`;
     }
 
-    function displayResponse(response) {
-        if (response.ok) {
-            linebreak(output);
-            output.value = "Message sent successfully";
-        } else {
-            output.value = `HTTP Status Code ${response.status}: ${response.statusText}`;
+    return baseUrl;
+}
 
-            response.text().then((text) => {
-                try {
-                    let json = JSON.parse(text);
+function displayResponse(response) {
+    if (response.ok) {
+        linebreak(output);
+        output.value = "Message sent successfully";
+    } else {
+        output.value = `HTTP Status Code ${response.status}: ${response.statusText}`;
 
-                    for (const [key, value] of Object.entries(json)) {
-                        linebreak(output);
-                        output.append(`${key}: ${value}`);
-                    }
-                } catch {
+        response.text().then((text) => {
+            try {
+                let json = JSON.parse(text);
+
+                for (const [key, value] of Object.entries(json)) {
                     linebreak(output);
-                    output.append(text);
+                    output.append(`${key}: ${value}`);
                 }
-            });
-        }
+            } catch {
+                linebreak(output);
+                output.append(text);
+            }
+        });
     }
+}
 
-    function linebreak(element) {
-        element.append(document.createElement("br"));
-    }
-})();
+function linebreak(element) {
+    element.append(document.createElement("br"));
+}
