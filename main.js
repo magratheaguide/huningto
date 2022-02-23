f95417748264fdce1cca1ef9c480c8645b742337
thewildmage <cecilia@hey.com>
2022-02-22 21:08:28 -0700

    check for js, add noscript warning

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+import * as JsCheck from "/modules/js-check.js";
 import * as Send from "/modules/send.js";
 import * as ConditionalInput from "/modules/conditional-input.js";
+
+JsCheck.setJsAvailable();
 
 const form = document.querySelector("form");
 const output = document.querySelector("output");
